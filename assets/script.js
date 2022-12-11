71b5514c8cc96510c855cf54caeaada8740c776d
jakester101 <55941541+jakester101@users.noreply.github.com>
2022-12-10 21:44:18 -0500

    added functions to js

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,32 @@
-var topic = ["HTML", "CSS", "Git", "JavaScript"];
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for(let i=0; i<topic.length; i++){
 
-    console.log(topic[i]);
+
+console.log("Here are the topics we learned through Prework:");
+
+function listTopics(){
+    for(let i=0; i<topics.length; i++){
+
+        console.log(topics[i]);
+    }
+}
+
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+      console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
 }
 
 
+listTopics();
+selectTopic();
 
-/*
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
-}
-*/
